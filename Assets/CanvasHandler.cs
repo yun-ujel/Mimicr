b3dbae9741b2b9785eac2e2e30ce532d9952f99c
yun-ujel <dhall2022@elam.co.uk>
2023-01-16 16:02:56 +0000

    Updated CanvasHandler
    trying to change canvas bounds based on the anchors of the window

--- Assets/CanvasHandler.cs
+++ Assets/CanvasHandler.cs
@@ -27,24 +27,48 @@ public class CanvasHandler : MonoBehaviour
         GameObject newWindow = Instantiate(windowToInstantiate, transform);
 
         // Randomize position of the object
-        RectTransform wRectTransform = newWindow.GetComponent<RectTransform>();
+        RectTransform rectTransform = newWindow.GetComponent<RectTransform>();
+        Vector2 remappedAnchor = new Vector2
+        (
+            -((rectTransform.anchorMax.x * 2) - 1),
+            -((rectTransform.anchorMax.y * 2) - 1)
+        );
 
-
-        Debug.Log(wRectTransform.anchorMax.Equals(wRectTransform.anchorMin));
-        
-
-        if (wRectTransform.anchorMax.Equals(wRectTransform.anchorMin))
-        {
-            wRectTransform.anchoredPosition = new Vector2
-                (
-                    canvasRectTransform.sizeDelta.x * ((wRectTransform.anchorMax.x * -1) + 1),
-                    canvasRectTransform.sizeDelta.y * ((wRectTransform.anchorMax.y * -1) + 1)
-                );
-        }
+        rectTransform.anchoredPosition = new Vector2
+        (
+            Random.Range
+            (
+                0f, 
+                canvasRectTransform.sizeDelta.x
+            ) * remappedAnchor.x,
+            Random.Range
+            (
+                0f, 
+                canvasRectTransform.sizeDelta.y
+            ) * remappedAnchor.y
+        );
     }
-
     private float Remap(float inputValue, float inMin, float inMax, float outMin, float outMax)
     {
         return (inputValue - inMin) / (inMax - inMin) * (outMax - outMin) + outMin;
     }
+
+    
+
+
+    //                                                  Anchors: 1, 0
+    // Bottom Right Min Range: -300, +300   Required Multiplier: -1, +1
+    // Bottom Right Max Range: +0, +0       Required Multiplier: 0, 0
+
+    //                                                  Anchors: 1, 1
+    // Top Right Min Range: -300, +0        Required Multiplier: -1, 0
+    // Top Right Max Range: +0, +300        Required Multiplier: 0, +1
+
+    //                                                  Anchors: 0, 0
+    // Bottom Left Min Range: +0, +300      Required Multiplier: 0, +1
+    // Bottom Left Max Range: -300, +0      Required Multiplier: -1, 0
+
+    //                                                  Anchors: 0, 1
+    // Top Left Min Range: +0, +0           Required Multiplier: 0, 0
+    // Top Left Max Range: -300, +300       Required Multiplier: -1, +1
 }
