bf23d2384f70c49b7d83376680a63ed55f5cb11a
yun-ujel <dhall2022@elam.co.uk>
2023-01-16 21:08:00 +0000

    started simplifying weird canvas calculations

--- Assets/CanvasHandler.cs
+++ Assets/CanvasHandler.cs
@@ -13,9 +13,13 @@ public class CanvasHandler : MonoBehaviour
         canvasRectTransform = GetComponent<RectTransform>();
     }
     
-    void Update()
+    void Start()
     {
-        
+        foreach (GameObject window in windowsToOpen)
+        {
+            RectTransform rT = window.GetComponent<RectTransform>();
+            Debug.Log(GetScreenPosition(rT));
+        }
     }
 
     public void InstantiateWindow(int windowIndex)
@@ -27,63 +31,28 @@ public class CanvasHandler : MonoBehaviour
         GameObject newWindow = Instantiate(windowToInstantiate, transform);
 
         // Randomize position of the object
-        RectTransform rectTransform = newWindow.GetComponent<RectTransform>();
-        Vector2 remappedAnchor = new Vector2
-        (
-            -((rectTransform.anchorMax.x * 2) - 1),
-            -((rectTransform.anchorMax.y * 2) - 1)
-        );
-
-        Vector2 minPosition = new Vector2
-        (
-            ((0f + (rectTransform.sizeDelta.x * (-rectTransform.anchorMax.x))) * remappedAnchor.x) * remappedAnchor.x,
-            0f + (rectTransform.sizeDelta.y * (-rectTransform.anchorMax.y + 1f)) * remappedAnchor.y
-        );
-        Vector2 maxPosition = new Vector2
-        (
-            (canvasRectTransform.sizeDelta.x + (rectTransform.sizeDelta.x * (rectTransform.anchorMax.x - 1f))) * remappedAnchor.x, 
-            (canvasRectTransform.sizeDelta.y + ((rectTransform.sizeDelta.y * rectTransform.anchorMax.y) * remappedAnchor.y)) * remappedAnchor.y
-        );
-
-        Debug.Log("Min Position:" + minPosition.ToString());
-        Debug.Log("Max Position:" + maxPosition.ToString());
-
-
-        rectTransform.anchoredPosition = new Vector2
-        (
-            Random.Range
-            (
-                minPosition.x, 
-                maxPosition.x
-            ),
-            Random.Range
-            (
-                minPosition.y,
-                maxPosition.y
-            )
-        );
+        RectTransform rT = newWindow.GetComponent<RectTransform>();
     }
+
+    public Vector2 GetScreenPosition(RectTransform rectTransform)
+    {
+        Vector2 basePivotPosition = new Vector2
+            (
+                rectTransform.anchoredPosition.x - (rectTransform.sizeDelta.x * rectTransform.pivot.x),
+                rectTransform.anchoredPosition.y - (rectTransform.sizeDelta.y * rectTransform.pivot.y)
+            );
+
+        Vector2 returnedPosition = new Vector2
+        (
+            basePivotPosition.x + (canvasRectTransform.sizeDelta.x * rectTransform.anchorMax.x),
+            basePivotPosition.y + (canvasRectTransform.sizeDelta.y * rectTransform.anchorMax.y)
+        );
+
+        return returnedPosition;
+    }
+
     private float Remap(float inputValue, float inMin, float inMax, float outMin, float outMax)
     {
         return (inputValue - inMin) / (inMax - inMin) * (outMax - outMin) + outMin;
     }
-
-    
-
-
-    //                                                  Anchors: 1, 0
-    // Bottom Right Min Range: -300, +300   Required Multiplier: -1, +1
-    // Bottom Right Max Range: +0, +0       Required Multiplier: 0, 0
-
-    //                                                  Anchors: 1, 1
-    // Top Right Min Range: -300, +0        Required Multiplier: -1, 0
-    // Top Right Max Range: +0, +300        Required Multiplier: 0, +1
-
-    //                                                  Anchors: 0, 0
-    // Bottom Left Min Range: +0, +300      Required Multiplier: 0, +1
-    // Bottom Left Max Range: -300, +0      Required Multiplier: -1, 0
-
-    //                                                  Anchors: 0, 1
-    // Top Left Min Range: +0, +0           Required Multiplier: 0, 0
-    // Top Left Max Range: -300, +300       Required Multiplier: -1, +1
 }
