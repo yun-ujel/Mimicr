ff9c4a6881afdcbbccbdd9d2dc3077b4922f5c6a
yun-ujel <dhall2022@elam.co.uk>
2023-01-25 10:44:28 +0000

    added left and right side message prefabs
    variable width (based on text) is not currently a priority, no matter how hard i tried to make it work

--- Assets/Scripts/Minigames/Messenger.cs
+++ Assets/Scripts/Minigames/Messenger.cs
@@ -1,25 +1,34 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class Messenger : MonoBehaviour
 {
-    [SerializeField] private GameObject textBubble;
+    [SerializeField] private GameObject leftTextBubble;
+    [SerializeField] private GameObject rightTextBubble;
 
-    void InstantiateMessage(string newText)
+    [SerializeField] private string messageText;
+    void InstantiateMessage(string newText, GameObject gameObject)
     {
-        GameObject messageObject = Instantiate(textBubble, transform);
+        GameObject messageObject = Instantiate(gameObject, transform);
         TextMeshProUGUI message = messageObject.GetComponentInChildren<TextMeshProUGUI>();
-        RectTransform rectTransform = messageObject.GetComponent<RectTransform>();
+        HorizontalLayoutGroup horizontalLayoutGroup = messageObject.GetComponentInChildren<HorizontalLayoutGroup>();
+        Rect rect = messageObject.GetComponent<RectTransform>().rect;
 
         message.text = newText;
+        Debug.Log(message.preferredWidth);
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            InstantiateMessage("oooooooooooooooaaaaaaaaaaaaa");
+            InstantiateMessage(messageText, leftTextBubble);
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            InstantiateMessage(messageText, rightTextBubble);
         }
     }
 }
