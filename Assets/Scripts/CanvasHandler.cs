eadb1d516803002df92e83cc964cdca539146b73
yun-ujel <dhall2022@elam.co.uk>
2023-01-26 11:46:09 +0000

    removed unused canvas methods

--- Assets/Scripts/CanvasHandler.cs
+++ Assets/Scripts/CanvasHandler.cs
@@ -87,30 +87,30 @@ public class CanvasHandler : MonoBehaviour
     }
 
 
-    public void OnCursorEnter(int mode)
+    public void OnCursorEnter(int mode) // Adds a cursor indicator, called when hovering over Resize bars
     {
-        cursorRenderer.color = new Color
+        cursorRenderer.color = new Color // Making the indicator visible
         (
             cursorRenderer.color.r,
             cursorRenderer.color.g,
             cursorRenderer.color.b,
             1f
         );
-        if (mode == 3 || mode == 5) // Left or Right
+        if (mode == 3 || mode == 5)                                      // Left or Right (resizing width)
         {
-            cursor.transform.rotation = Quaternion.identity;
+            cursor.transform.rotation = Quaternion.identity;             // Indicator pointed upwards
         }
-        if (mode == 0 || mode == 8) // Top Left or Bottom Right
+        if (mode == 0 || mode == 8)                                      // Top Left or Bottom Right (resizing width and height)
         {
-            cursor.transform.rotation = Quaternion.Euler(0f, 0f, 135);
+            cursor.transform.rotation = Quaternion.Euler(0f, 0f, 135);   // Angled Indicator
         }
-        if (mode == 1 || mode == 7) // Top or Bottom
+        if (mode == 1 || mode == 7)                                      // Top or Bottom (resizing height)
         {
-            cursor.transform.rotation = Quaternion.Euler(0f, 0f, 90);
+            cursor.transform.rotation = Quaternion.Euler(0f, 0f, 90);    // Indicator pointed upwards
         }
-        if (mode == 2 || mode == 6) // Top Right or Bottom Left
+        if (mode == 2 || mode == 6)                                      // Top Right or Bottom Left (resizing width and height)
         {
-            cursor.transform.rotation = Quaternion.Euler(0f, 0f, 45);
+            cursor.transform.rotation = Quaternion.Euler(0f, 0f, 45);    // Angled Indicator
         }
     }
     public void OnCursorExit()
@@ -131,23 +131,12 @@ public class CanvasHandler : MonoBehaviour
 
 
 
-    public Vector2 FindSize(RectTransform rectTransform)
-    {
-        RectTransform parentTransform = rectTransform.parent.GetComponent<RectTransform>();
 
-        Vector2 returnedSizeDelta = new Vector2
-        (
-            (parentTransform.sizeDelta.x * Mathf.Abs(rectTransform.anchorMax.x - rectTransform.anchorMin.x)) + (rectTransform.offsetMax.x - rectTransform.offsetMin.x),
-            (parentTransform.sizeDelta.y * Mathf.Abs(rectTransform.anchorMax.y - rectTransform.anchorMin.y)) + (rectTransform.offsetMax.y - rectTransform.offsetMin.y)
-        );
-
-        return returnedSizeDelta;
-    }
     public Vector2 UnanchorPosition(RectTransform rectTransform)
     {
-        Vector2 transformSize = FindSize(rectTransform);
         RectTransform parentTransform = rectTransform.parent.GetComponent<RectTransform>();
-        // Method will only work if direct parent has an unstretched size
+        Vector2 transformSize = rectTransform.rect.size;
+        Vector2 parentTransformSize = parentTransform.rect.size;
 
         Vector2 basePivotPosition = new Vector2
             (
@@ -157,15 +146,15 @@ public class CanvasHandler : MonoBehaviour
 
         Vector2 baseAnchorPosition = new Vector2
         (
-            basePivotPosition.x + (parentTransform.sizeDelta.x * ((rectTransform.anchorMax.x + rectTransform.anchorMin.x) / 2f)),
-            basePivotPosition.y + (parentTransform.sizeDelta.y * ((rectTransform.anchorMax.y + rectTransform.anchorMin.y) / 2f))
+            basePivotPosition.x + (parentTransformSize.x * ((rectTransform.anchorMax.x + rectTransform.anchorMin.x) / 2f)),
+            basePivotPosition.y + (parentTransformSize.y * ((rectTransform.anchorMax.y + rectTransform.anchorMin.y) / 2f))
         );
 
         return baseAnchorPosition;
     }
     public Vector2 ReanchorPosition(RectTransform rectTransform, Vector2 unanchoredPosition)
     {
-        Vector2 transformSize = FindSize(rectTransform);
+        Vector2 transformSize = rectTransform.rect.size;
         RectTransform parentTransform = rectTransform.parent.GetComponent<RectTransform>();
         // Method will only work if direct parent has an unstretched size
 
@@ -182,72 +171,6 @@ public class CanvasHandler : MonoBehaviour
         );
 
         return rePivotPosition;
-    }
-    public Vector2 FullyUnanchorPosition(RectTransform rectTransform)
-    {
-        List<RectTransform> testListOfParents = new List<RectTransform>();
-        RectTransform testTransform = rectTransform;
-        testListOfParents.Add(testTransform);
-        while (testTransform.parent != null)
-        {
-            testTransform = testTransform.parent.gameObject.GetComponent<RectTransform>();
-            testListOfParents.Add(testTransform);
-        }
-
-        RectTransform[] listOfParents = testListOfParents.ToArray();
-        Vector2[] listOfSizes = new Vector2[listOfParents.Length];
-
-        Vector2 lastCalculatedSize = listOfParents[listOfParents.Length - 1].sizeDelta;
-
-        for (int i = listOfSizes.Length - 1; i > 0; i--)
-        {
-            listOfSizes[i] = lastCalculatedSize;
-
-            lastCalculatedSize = new Vector2
-            (
-                (lastCalculatedSize.x * Mathf.Abs(listOfParents[i - 1].anchorMax.x - listOfParents[i - 1].anchorMin.x)) + (listOfParents[i - 1].offsetMax.x - listOfParents[i - 1].offsetMin.x),
-                (lastCalculatedSize.y * Mathf.Abs(listOfParents[i - 1].anchorMax.y - listOfParents[i - 1].anchorMin.y)) + (listOfParents[i - 1].offsetMax.y - listOfParents[i - 1].offsetMin.y)
-            );
-        }
-        listOfSizes[0] = lastCalculatedSize;
-        Vector2 relativePosition = new Vector2(0f, 0f);
-
-        for (int i = 0; i < listOfParents.Length - 1; i++)
-        {
-            relativePosition = new Vector2
-            (
-                relativePosition.x + (listOfParents[i].anchoredPosition.x - (listOfSizes[i].x * listOfParents[i].pivot.x)) + (listOfSizes[i + 1].x * ((listOfParents[i].anchorMax.x + listOfParents[i].anchorMin.x) / 2f)),
-                relativePosition.y + (listOfParents[i].anchoredPosition.y - (listOfSizes[i].y * listOfParents[i].pivot.y)) + (listOfSizes[i + 1].y * ((listOfParents[i].anchorMax.y + listOfParents[i].anchorMin.y) / 2f))
-            );
-        }
-
-        return relativePosition;
-    }
-    public Vector2 FullyFindSize(RectTransform rectTransform)
-    {
-        List<RectTransform> testListOfParents = new List<RectTransform>();
-        RectTransform testTransform = rectTransform;
-        testListOfParents.Add(testTransform);
-        while (testTransform.parent != null)
-        {
-            testTransform = testTransform.parent.gameObject.GetComponent<RectTransform>();
-            testListOfParents.Add(testTransform);
-        }
-
-        RectTransform[] listOfParents = testListOfParents.ToArray();
-
-        Vector2 lastCalculatedSize = listOfParents[listOfParents.Length - 1].sizeDelta;
-
-        for (int i = listOfParents.Length - 1; i > 0; i--)
-        {
-            lastCalculatedSize = new Vector2
-            (
-                (lastCalculatedSize.x * Mathf.Abs(listOfParents[i - 1].anchorMax.x - listOfParents[i - 1].anchorMin.x)) + (listOfParents[i - 1].offsetMax.x - listOfParents[i - 1].offsetMin.x),
-                (lastCalculatedSize.y * Mathf.Abs(listOfParents[i - 1].anchorMax.y - listOfParents[i - 1].anchorMin.y)) + (listOfParents[i - 1].offsetMax.y - listOfParents[i - 1].offsetMin.y)
-            );
-        }
-
-        return lastCalculatedSize;
     }
 
     private float Remap(float inputValue, float inMin, float inMax, float outMin, float outMax)
